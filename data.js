522b24044f31b21607c8fe0603d189fe75b3a6e5
Joaquin <gustavo.chazarreta10@gmail.com>
2023-03-13 14:26:45 -0300

    Update data.js

--- data.js
+++ data.js
@@ -3,10 +3,11 @@ async function cargaProd (){ fetch("productos.json")
   .then((response) => response.json())
   .then((data) => 
    {
-    for ( let i of data ){
+    for ( let i of data )
+    {
     arrayDetalles.push(i);
-   }}
-  )
+   }})
+   .then(()=>mostrarProductos(arrayDetalles))
     .catch(e => {
       console.log(e);
   });;
